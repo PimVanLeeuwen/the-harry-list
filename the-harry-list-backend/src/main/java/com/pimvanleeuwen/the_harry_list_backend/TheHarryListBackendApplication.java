6d1f2c3eea996be81ea91bf5bcdd3b164870dd03
Pim van Leeuwen <git@pvleeuwen.com>
2025-09-26 17:28:42 +0200

    Initial commit

---
+++ the-harry-list-backend/src/main/java/com/pimvanleeuwen/the_harry_list_backend/TheHarryListBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pimvanleeuwen.the_harry_list_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheHarryListBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheHarryListBackendApplication.class, args);
+	}
+
+}
---
+++ the-harry-list-backend/src/test/java/com/pimvanleeuwen/the_harry_list_backend/TheHarryListBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pimvanleeuwen.the_harry_list_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheHarryListBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
